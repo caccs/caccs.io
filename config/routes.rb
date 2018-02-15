95085d1081e035cc61519157ee0be9ab6703b0cc
darkkgreen <grr.pride@gmail.com>
2018-02-15 12:10:43 -0200

    added new Gems, pages and skeleton. Also, fix'd some routes and files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  get  '/about',    to: 'pages#about'
+  get  '/contact',  to: 'pages#contact'
+  get  '/calendar', to: 'calendar#calendar'
 
-  get 'pages/about'
-
-  get 'pages/contact'
-
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
