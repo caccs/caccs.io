a1768d038c68e454a8248e8c0cdf997c9487b404
darkkgreen <grr.pride@gmail.com>
2018-02-22 14:52:18 -0300

    Added flash message (dunno why there's no layout using bootstrap) when an email has been sent

--- Gemfile
+++ Gemfile
@@ -15,7 +15,6 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails'
 
-
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara', '~> 2.13'
--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -5,5 +5,7 @@ class EmailController < ApplicationController
 	  	mensagem = params["person"]["message"].to_s
 
 	  	EmailMailer.contact_email(nome, email, mensagem).deliver_now
+	  	flash[:notice] = "Email enviado com sucesso!"
+	  	redirect_to contact_path
 	end
 end
