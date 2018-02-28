0e46dacaa1b00bfab876da5859d81b78a6b21ab2
darkkgreen <grr.pride@gmail.com>
2018-02-28 15:53:18 -0300

    Added new front end Files, gems and infos to our About page
    The About page was empty, so I filled it in with information about the
    project, our entity and our developers, aswell as a little changelog.
    
    The page works basically with HTML and CSS. Used Font Awesome CSS files
    to edit images and icons.

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails'
 gem 'mailjet'
+gem "parsley-rails"
+gem "font-awesome-rails"
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require_tree .
 //= require jquery
 //= require jquery_ujs
 //= require rails-ujs
+//= require parsley
 //= require turbolinks
-//= require_tree .
