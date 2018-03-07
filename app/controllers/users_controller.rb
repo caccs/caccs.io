ff2ad18c34ab9a15728e9c71ca5649a73eea4f3a
darkkgreen <grr.pride@gmail.com>
2018-03-07 16:57:54 -0300

    Need some fixes here and there but soon this user page will be whole

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
-	def user_params
-		params.require(:user).permit(:name, :login, :email, :password, :password_confirmation)
-	end
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 	def new
 		@user = User.new
@@ -9,14 +7,31 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.new(user_params)
-		if !@user.save
-			flash[:notice] = "Problem signing up"
+		if @user.save
+			flash[:notice] = "User created"
+		else
+			flash[:notice] = "User not created"
 		end
 
-		render "new"
+		render "index"
 	end
 
 	def index
 		@users = User.all
 	end
+
+	def destroy
+		@user.destroy
+
+		render "index"
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:name, :login, :email, :password, :password_confirmation)
+		end
+
+		def set_user
+	      @user = User.find(params[:id])
+	    end
 end
