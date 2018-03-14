24d6227f3dbbd8c7debfd98f8cc05751238c7055
darkkgreen <grr.pride@gmail.com>
2018-03-14 13:15:42 -0300

    Added persistance to cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
 	has_secure_password
 	before_save { self.email = email.downcase }
 	before_save { self.login = login.downcase }
@@ -7,4 +8,28 @@ class User < ApplicationRecord
 	validates :login, presence: true, uniqueness: true, length: { in: 6..20, too_short: "Login muito curto", too_long: "Login muito longo" }
 	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
 	validates :password, presence: true, confirmation: true, length: { minimum: 6 }
+
+	def User.digest(string)
+	    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+	                                                  BCrypt::Engine.cost
+	    BCrypt::Password.create(string, cost: cost)
+	  end
+
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+	    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	def forget
+		update_attribute(:remember_digest, nil)
+	end
 end
