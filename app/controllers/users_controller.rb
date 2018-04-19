7aead9ff94391fb6d06a674c44321f002ec03748
darkkgreen <grr.pride@gmail.com>
2018-04-19 15:39:05 -0300

    Added event model. Still rewriting everything

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,66 +1,68 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:show, :edit, :update, :destroy]
-	before_action :reload, only: [:index, :create, :destroy, :update]
-	before_action :permission
+  before_action :set_user, only: %i[show edit update destroy]
+  before_action :reload, only: %i[index create destroy update]
+  before_action :permission
 
-	def index
-		reload
-	end
+  def index
+    reload
+  end
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save(user_params)
-			redirect_to users_path
-		else
-			redirect_to new_user_path
-		end
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save(user_params)
+      redirect_to users_path
+    else
+      redirect_to new_user_path
+    end
+  end
 
-	def show
-	end
+  def show
+  end
 
-	def destroy
-		if @user.destroy
-			@user = nil
-			redirect_to users_path
-		end
-	end
+  def destroy
+    if @user.destroy
+      @user = nil
+      redirect_to users_path
+    end
+  end
 
-	def edit
-		@user = User.find(params[:id])
-	end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-	def update
-	    @user = User.find(params[:id])
-	    if @user = @user.update_attributes(user_params)
-	    	flash[:success] = "Profile atualizado com sucesso"
-    		redirect_to users_path
-	    else
-	    	flash[:danger] = "Problema com atualização encontrada"
-	    	redirect_to edit_user_path
-	    end
-	end
+  def update
+    @user = User.find(params[:id])
+    if @user == @user.update_attributes(user_params)
+      flash[:success] = 'Profile atualizado com sucesso'
+      redirect_to users_path
+    else
+      flash[:danger] = 'Problema com atualização encontrada'
+      redirect_to edit_user_path
+    end
+  end
 
-	private
-		def user_params
-			params.require(:user).permit(:name, :login, :email, :password, :password_confirmation)
-		end
+  private
 
-		def set_user
-	      	@user = User.find(params[:id])
-	    end
+  def user_params
+    params.require(:user).permit(:name, :login, :email, :password, :password_confirmation)
+  end
 
-	    def reload
-	    	@users = User.order(:name).page params[:page]
-	    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-	    def permission
-	    	if !logged_in?
-	    		render :file => 'public/401'
-	    	end
-	    end
+  def reload
+    @users = User.order(:name).page params[:page]
+  end
+
+  def permission
+    if !logged_in?
+      render :file => 'public/401'
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,35 +1,38 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
-	has_secure_password
-	before_save { self.email = email.downcase }
-	before_save { self.login = login.downcase }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
-	validates :name, presence: true
-	validates :login, presence: true, uniqueness: true, length: { in: 6..20, too_short: "Login muito curto", too_long: "Login muito longo" }
-	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
-	validates :password, presence: true, confirmation: true, length: { minimum: 6 }, :on => :create
+  attr_accessor :remember_token
+  has_secure_password
+  before_save { self.email = email.downcase }
+  before_save { self.login = login.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
+  validates :name, presence: true
+  validates :login, presence: true, uniqueness: true, length: { in: 6..20, too_short: "Login muito curto", too_long: "Login muito longo" }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 }, :on => :create
 
-	def User.digest(string)
-	    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-	                                                  BCrypt::Engine.cost
-	    BCrypt::Password.create(string, cost: cost)
-	  end
+  def self.digest(string)
+    if ActiveModel::SecurePassword.min_cost
+      cost = BCrypt::Engine::MIN_COST
+    else
+      cost = BCrypt::Engine.cost
+    end
+    BCrypt::Password.create(string, cost: cost)
+  end
 
-	def User.new_token
-		SecureRandom.urlsafe_base64
-	end
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
 
-	def remember
-		self.remember_token = User.new_token
-		update_attribute(:remember_digest, User.digest(remember_token))
-	end
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 
-	def authenticated?(remember_token)
-		return false if remember_digest.nil?
-	    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-	end
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 
-	def forget
-		update_attribute(:remember_digest, nil)
-	end
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
