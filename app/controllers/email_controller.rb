96fa2e18fcb8b96b7e1074c584ea35ff33c46c42
Gustavo Rodrigues <grr.pride@gmail.com>
2018-05-09 11:17:26 -0300

    Fix to DangerCI warnings. Some of them will be ignored

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,8 +1,11 @@
+# Class that controls how we send emails
 class EmailController < ApplicationController
   def form
-    nome = params['person']['name'].to_s
-    email = params['person']['email'].to_s
-    mensagem = params['person']['message'].to_s
+    parametros = params['person']
+
+    nome = parametros['name'].to_s
+    email = parametros['email'].to_s
+    mensagem = parametros['message'].to_s
 
     EmailMailer.contact_email(nome, email, mensagem).deliver_now!
     flash[:notice] = 'Email enviado com sucesso!'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,10 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(login: params[:session][:login].downcase)
-    if user && user.authenticate(params[:session][:password])
+    sessao = params[:session]
+
+    user = User.find_by(login: sessao[:login].downcase)
+    if user && user.authenticate(sessao[:password])
       log_in user
       remember user
       redirect_to user
